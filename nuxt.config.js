32fca8aac0422c0a3ca5afce3cb75ad21f27aa5e
Sultan <im.sultaan05@gmail.com>
2019-12-08 15:46:21 +0700

    install vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,6 +44,7 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     // '@nuxtjs/eslint-module'
+    '@nuxtjs/vuetify',
   ],
   /*
    ** Nuxt.js modules
