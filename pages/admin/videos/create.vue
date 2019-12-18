bc6cd1fd34ac3c31f9a5a30f146e6fd3da19e4b2
Sultan <im.sultaan05@gmail.com>
2019-12-18 21:14:35 +0700

    change form create video

--- pages/admin/videos/create.vue
+++ pages/admin/videos/create.vue
@@ -1,23 +1,14 @@
 <template>
 	<v-container>
 		<h1>Video Create Page</h1>
-		<v-row>
-			<v-col md="3" cols="12">
-				<VideoEditForm :video="video" :saveVideo="createVideo" buttonText="Create Video"></VideoEditForm>
-			</v-col>
-			<v-col md="9" cols="12">
-				<ListVideo :video="video" />
-			</v-col>
-		</v-row>
+		<VideoEditForm :video="video" :saveVideo="createVideo" buttonText="Create Video"></VideoEditForm>
 	</v-container>
 </template>
 
 <script>
-import ListVideo from "@/components/ListVideo";
 import VideoEditForm from "@/components/VideoEditForm";
 export default {
 	components: {
-		ListVideo,
 		VideoEditForm
 	},
 	data() {
@@ -28,7 +19,7 @@ export default {
 	methods: {
 		async createVideo() {
 			let video = await this.$store.dispatch("videos/create", this.video);
-			this.$router.push(`/watch/${video.id}`);
+			this.$router.push(`/admin/videos/${video.id}`);
 		}
 	}
 };
