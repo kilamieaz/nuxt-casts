64dfbe5904253508722f7a946389132dae6201d1
Sultan <im.sultaan05@gmail.com>
2019-12-18 21:12:56 +0700

    component show duration and published on

---
+++ components/VideoByLine.vue
@@ -0,0 +1,21 @@
+<template>
+	<em>
+		<DurationDisplay :duration="video.duration" />
+		&nbsp; | &nbsp;
+		Published on {{ video.created_at }}
+	</em>
+</template>
+
+<script>
+import DurationDisplay from "@/components/DurationDisplay";
+
+export default {
+	components: {
+		DurationDisplay
+	},
+	props: ["video"]
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
