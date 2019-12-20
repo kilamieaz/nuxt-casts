a42221dc882faaedc44c30ceead70f7f6e079a73
Sultan <im.sultaan05@gmail.com>
2019-12-20 16:05:35 +0700

    improved UI for editing markdown

--- components/VideoEditForm.vue
+++ components/VideoEditForm.vue
@@ -7,13 +7,29 @@
 			counter="50"
 			:rules="[required('name'), minLength('name', 5), maxLength('name', 50)]"
 		/>
-		<v-textarea
-			v-model="video.description"
-			label="Description"
-			counter="true"
-			:rules="[required('description'), minLength('description', 20)]"
-		/>
-		<v-textarea v-model="video.code_summary" label="Code Summary" />
+
+		<v-row>
+			<v-col cols="12" sm="6">
+				<v-textarea
+					v-model="video.description"
+					label="Description"
+					counter="true"
+					rows="9"
+					:rules="[required('description'), minLength('description', 20)]"
+				/>
+			</v-col>
+			<v-col cols="12" sm="6">
+				<MarkdownDisplay :markdown="video.description"></MarkdownDisplay>
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col cols="12" sm="6">
+				<v-textarea v-model="video.code_summary" label="Code Summary" rows="12" />
+			</v-col>
+			<v-col cols="12" sm="6">
+				<MarkdownDisplay :markdown="video.code_summary"></MarkdownDisplay>
+			</v-col>
+		</v-row>
 		<v-text-field v-model="video.duration" label="Duration (in seconds)"></v-text-field>
 		<DurationDisplay :duration="video.duration" />
 		<v-text-field
@@ -34,6 +50,7 @@
 <script>
 import validations from "@/utils/validations";
 import DurationDisplay from "@/components/DurationDisplay";
+import MarkdownDisplay from "@/components/MarkdownDisplay";
 export default {
 	data() {
 		return {
