0e3f81326ad27b057f70c197315e9160fecfdbf3
Sultan <im.sultaan05@gmail.com>
2020-01-24 17:13:12 +0700

    tags and played columns

--- components/VideoTable.vue
+++ components/VideoTable.vue
@@ -11,10 +11,28 @@
 		<template #item.duration="{value}">
 			<DurationDisplay :duration="value" />
 		</template>
+		<template #item.played="{item}">
+			<div class="green--text" v-if="videoIsPlayed(item.id)">
+				<v-row>
+					<font-awesome-icon icon="check" />
+				</v-row>
+			</div>
+			<div class="red--text" v-else>
+				<v-row>
+					<font-awesome-icon icon="check" />
+				</v-row>
+			</div>
+		</template>
+		<template #item.tags="{item}">
+			<span v-for="tag in item.tags" :key="tag.id">
+				<v-btn color="green lighten-2" class="mr-1" x-small :to="`/tags/${tag.id}`">{{ tag.name }}</v-btn>
+			</span>
+		</template>
 	</v-data-table>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import DurationDisplay from "@/components/DurationDisplay";
 import DateDisplay from "@/components/DateDisplay";
 export default {
@@ -23,11 +41,16 @@ export default {
 		DateDisplay
 	},
 	computed: {
+		...mapGetters({
+			videoIsPlayed: "user/videoIsPlayed"
+		}),
 		headers() {
 			return [
 				{ text: "Name", value: "name" },
 				{ text: "Date", value: "sortable_published_at" },
-				{ text: "Duration", value: "duration" }
+				{ text: "Duration", value: "duration" },
+				{ text: "Played", value: "played", sortable: false },
+				{ text: "Tags", value: "tags", sortable: false }
 			];
 		},
 		mungedVideos() {
