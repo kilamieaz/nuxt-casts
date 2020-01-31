3e1be40a8eb2c6ea94a7ca158902ead3a086be3f
Sultan <im.sultaan05@gmail.com>
2020-01-31 15:56:00 +0700

    vue custom filter data table

--- components/VideoTable.vue
+++ components/VideoTable.vue
@@ -1,51 +1,56 @@
 <template>
-	<v-data-table
-		:items="videos"
-		:headers="headers"
-		sort-by="published_at"
-		:sort-desc="true"
-		@click:row="goToVideo"
-		show-expand
-	>
-		<template #item.published_at="{item}">
-			<DateDisplay :date="item.published_at" />
-		</template>
-		<template #item.duration="{value}">
-			<DurationDisplay :duration="value" />
-		</template>
-		<template #item.played="{item}">
-			<div class="green--text" v-if="videoIsPlayed(item.id)">
-				<v-row>
-					<font-awesome-icon icon="check" />
-				</v-row>
-			</div>
-			<div class="red--text" v-else>
-				<v-row>
-					<font-awesome-icon icon="check" />
-				</v-row>
-			</div>
-		</template>
-		<template #item.tags="{item}">
-			<td @click.stop class="non-clickable">
-				<span v-for="tag in item.tags" :key="tag.id">
-					<v-btn color="green lighten-2" class="mr-1" x-small :to="`/tags/${tag.id}`">{{ tag.name }}</v-btn>
-				</span>
-			</td>
-		</template>
-		<template #expanded-item="{headers, item}">
-			<td :colspan="headers.length">
-				<v-row>
-					<v-col cols="12" md="4">
-						<VideoWatch :video="item"></VideoWatch>
-					</v-col>
-					<v-col cols="12" md="8">
-						<h1>{{ item.name }}</h1>
-						<MarkdownDisplay :markdown="item.description"></MarkdownDisplay>
-					</v-col>
-				</v-row>
-			</td>
-		</template>
-	</v-data-table>
+	<div>
+		<v-text-field v-model="search" label="Search" single-line hide-details></v-text-field>
+		<v-data-table
+			:items="videos"
+			:headers="headers"
+			sort-by="published_at"
+			:sort-desc="true"
+			@click:row="goToVideo"
+			:search="search"
+			:custom-filter="filter"
+			show-expand
+		>
+			<template #item.published_at="{item}">
+				<DateDisplay :date="item.published_at" />
+			</template>
+			<template #item.duration="{value}">
+				<DurationDisplay :duration="value" />
+			</template>
+			<template #item.played="{item}">
+				<div class="green--text" v-if="videoIsPlayed(item.id)">
+					<v-row>
+						<font-awesome-icon icon="check" />
+					</v-row>
+				</div>
+				<div class="red--text" v-else>
+					<v-row>
+						<font-awesome-icon icon="times" />
+					</v-row>
+				</div>
+			</template>
+			<template #item.tags="{item}">
+				<td @click.stop class="non-clickable">
+					<span v-for="tag in item.tags" :key="tag.id">
+						<v-btn color="green lighten-2" class="mr-1" x-small :to="`/tags/${tag.id}`">{{ tag.name }}</v-btn>
+					</span>
+				</td>
+			</template>
+			<template #expanded-item="{headers, item}">
+				<td :colspan="headers.length">
+					<v-row>
+						<v-col cols="12" md="4">
+							<VideoWatch :video="item"></VideoWatch>
+						</v-col>
+						<v-col cols="12" md="8">
+							<h1>{{ item.name }}</h1>
+							<MarkdownDisplay :markdown="item.description"></MarkdownDisplay>
+						</v-col>
+					</v-row>
+				</td>
+			</template>
+		</v-data-table>
+	</div>
 </template>
 
 <script>
@@ -60,6 +65,9 @@ export default {
 		DateDisplay,
 		VideoWatch,
 		MarkdownDisplay
+	},
+	data() {
+		return { search: "" };
 	},
 	computed: {
 		...mapGetters({
@@ -78,6 +86,17 @@ export default {
 	methods: {
 		goToVideo(item) {
 			this.$router.push(`/watch/${item.id}`);
+		},
+		filter(value, search, item) {
+			let inName = RegExp(search, "i").test(item.name);
+			let inTags = false;
+			item.tags.map(tag => {
+				let inTag = RegExp(search, "i").test(tag.name);
+				if (inTag) {
+					inTags = true;
+				}
+			});
+			return inName || inTags;
 		}
 	},
 	props: ["videos"]
